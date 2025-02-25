fed619ba5481013c61874b977ed09bb621e3057f
morbid240 <hatredpartisan666@gmail.com>
2025-02-25 07:52:17 -0800

    Final makefiles, hs mergesort

--- HaskellTest/app/MergeSort.hs
+++ HaskellTest/app/MergeSort.hs
@@ -1,28 +1,20 @@
---merge sort in haskell
+module MergeSort (msort) where
 
-module MergeSort(
-    merge,
-    msort
-) where
-
--- MERGE IMPLEMENTATION takes two sorted lists and returns a single merged sorted list. Base cases: when one list is empty, returns non empty list given in paramters. recursive case: recursively conpare first elements of two lists- if left element is smaller or equal, prepend that element to second list. ofherwise prepend second element to fitst list. continue until one list is empty 
-merge :: Ord a => [a] -> [a] -> [a] 
--- base cases
-merge [] right = right
-merge left [] = left 
--- recursive case
-merge (x:xs) (y:ys) = 
-   if x <= y 
-   then x : merge xs (y:ys)
-   else y : merge (x:xs) ys
-
-
--- MERGESORT IMPLEMENTATION takes an unsorted list and recursivly partition subarrays until empty or single element list (base cases) returns sorted list. Partitions by calculating middle and assigning left and right subarrays. note: take and drop non destructive, will not change original list 
+-- MERGESORT IMPLEMENTATION
+-- Takes an unsorted list and recursively partitions subarrays until empty or a single element list (base cases), then returns a sorted list.
+-- Partitions by calculating the middle and assigning left and right subarrays.
+-- Note: `take` and `drop` are non-destructive and do not modify the original list.
 msort :: Ord a => [a] -> [a]
-msort [] = [] --empty list base case
-msort [x] = [x] --single element base case
-msort xs = merge (msort left) (msort right) 
-    where 
-        middle = length xs `div` 2
-        left = take middle xs
-        right = drop middle xs
+msort [] = []  -- empty list base case
+msort [x] = [x]  -- single element base case
+msort xs = merge (msort left) (msort right)  -- recursively sort and merge
+  where
+    middle = length xs `div` 2
+    left = take middle xs
+    right = drop middle xs
+    -- MERGE Takes two sorted lists and merges them into one sorted list.
+    merge [] right = right  -- base case: one list is empty
+    merge left [] = left    -- base case: other list is empty
+    merge (x:xs) (y:ys)
+      | x <= y    = x : merge xs (y:ys)  -- recursive case: compare and prepend the smaller element
+      | otherwise = y : merge (x:xs) ys
